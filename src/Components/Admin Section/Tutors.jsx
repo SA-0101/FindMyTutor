f9e3441bb306ff9d68e9d6b90500e85367285bbc
shaan Aslam <shaanaslam0101@gmail.com>
2025-06-10 09:41:29 +0500

    update

---
+++ src/Components/Admin Section/Tutors.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Tutors() {
+  return (
+    <div>
+            Tutors
+    </div>
+  )
+}
+
+export default Tutors
---
+++ src/Components/Admin Section/admin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function admin() {
+  return (
+    <div>
+            components will render here
+    </div>
+  )
+}
+
+export default admin
