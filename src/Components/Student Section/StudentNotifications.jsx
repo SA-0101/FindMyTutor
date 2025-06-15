48c1041e9c92b2b3637715cf47d71ba83c8b4fbc
shaan Aslam <shaanaslam0101@gmail.com>
2025-06-16 00:35:56 +0500

    update

--- src/Components/Student Section/StudentNotifications.jsx
+++ src/Components/Student Section/StudentNotifications.jsx
@@ -48,6 +48,38 @@ function StudentNotifications() {
           getNotifications();
     },[])
 
+
+    const removeNotification=async (notification)=> {
+  
+          try {
+            const response = await fetch(`${BASE_URL}/removeNotification/${notification._id}`,{
+              method:"DELETE",
+              headers:{
+                'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+              }
+
+            });
+            
+            const responsedata = await response.json();
+      
+            if (response.ok) {
+
+              alert("Notification Removed")
+              getNotifications();
+              console.log(responsedata)
+            }
+            else{
+                alert(responsedata.message)
+            }
+
+          } catch (error) {
+            console.error('Error:', error);
+          }
+        
+      }
+
+
     {/*Function for Separating Date and Time */}
     function splitDateTime(datetimeString) {
        const date = new Date(datetimeString);
@@ -58,11 +90,12 @@ function StudentNotifications() {
     }
 
   return (
-    <div className="w-full bg-green-300">
+    <div className="w-full">
 
           <div className="flex flex-col justify-center items-center gap-5">
 
-            <div>📩 Your Notifications</div>
+            <div className="text-2xl font-bold">🔔 Your Notifications</div>
+              <hr />
 
             {
               notifications.length===0 ? <div>No Notifications for You</div> :
@@ -70,14 +103,14 @@ function StudentNotifications() {
 
               return <div key={index} className="w-full flex flex-col items-start rounded-lg gap-2 px-3 py-2 border border-black">
 
-              <div className="w-full flex justify-between">
-                <h1>From: {notification.senderType}</h1>
+              <div className="w-full flex justify-between bg-[#FAFAFA]">
+                <h1>From: <span className="font-semibold">{notification.senderType}</span></h1>
                 <h1>{splitDateTime(notification.createdAt).dateOnly} at {splitDateTime(notification.createdAt).timeOnly}</h1>
               </div>
 
-              <h1>{notification.title}</h1>
+              <h1 className="text-lg font-semibold">{notification.title}</h1>
               <h1>{notification.message}</h1>
-              <button className="px-4 py-1 rounded-lg text-white font-semibold bg-red-500">Delete</button>
+              <button className="px-4 py-1 rounded-lg text-white font-semibold bg-red-500" onClick={()=>{removeNotification(notification)}}>Delete</button>
 
             </div>
 
