3630943326f2802a5fe3f2c7c93c104ed2fac79b
shaan Aslam <shaanaslam0101@gmail.com>
2025-06-15 16:18:55 +0500

    update

--- src/Components/Student Section/NearbyTeachers.jsx
+++ src/Components/Student Section/NearbyTeachers.jsx
@@ -1,4 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css"; // Leaflet CSS
+
+// ✅ Fix the default marker icon (do this ONCE)
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
+});
 
 function NearbyTeachers() {
   return (
