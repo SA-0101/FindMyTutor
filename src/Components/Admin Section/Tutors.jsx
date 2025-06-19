88dcdb1976fa573c9bfddb7c9a77f2a7927c3cb4
shaan Aslam <shaanaslam0101@gmail.com>
2025-06-19 08:54:35 +0500

    update

--- src/Components/Admin Section/Tutors.jsx
+++ src/Components/Admin Section/Tutors.jsx
@@ -1,10 +1,11 @@
 import { FaChalkboardTeacher} from 'react-icons/fa';
+import { FaSearch } from 'react-icons/fa';
 
 
 function Tutors() {
   return (
     <div className='w-full flex flex-col items-center gap-5 px-5 py-2'>
-            <div className='bg-green-200 flex flex-col items-center px-5 py-2'>
+            <div className='bg-green-200 flex flex-col items-center gap-2 px-5 py-2'>
                 <FaChalkboardTeacher size={30} color='blue'/>
                 <h1 className='text-4xl font-bold'>Teacher Management</h1>
                 <h1>Manage and Organize your teaching Staff</h1>
@@ -13,7 +14,11 @@ function Tutors() {
               <h1>All Teachers</h1>
               <h1>6</h1>
             </div>
-            <div className='bg-yellow-200 w-[100px] h-20'></div>
+            <div className='w-[300px] flex justify-center items-center gap-2 border border-black rounded-sm px-2 py-1'>
+                <FaSearch />
+               <input className='w-full outline-0' type="text" name="" id="" placeholder='Search by name or subject...'/>
+            </div>
+           
             <div className='bg-fuchsia-200 w-full h-36'></div>
     </div>
   )
