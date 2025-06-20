874866161a24ea96346f0236c565cdfbb99e10b0
shaan Aslam <shaanaslam0101@gmail.com>
2025-06-20 08:59:01 +0500

    update

--- src/Components/Admin Section/adminCategories.jsx
+++ src/Components/Admin Section/adminCategories.jsx
@@ -1,27 +1,64 @@
 import { Plus, Trash2, Tag, FileText, Settings, Folder } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function AdminCategories() {
 
     const BASE_URL="http://localhost:8000/tutor"
 
+    const [categorydata,setCategorydata]=useState([])
     const [categoryname,setCategoryname]=useState("")
     const [description,setDescription]=useState("")
-
     const token=localStorage.getItem('token')
 
-    const categoryData={
+    const getCategory=async ()=>{
+
+       try {
+                const response = await fetch(`${BASE_URL}/getAllCategories`,{
+                  method:"GET",
+                  headers:{
+                    'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                  }
+    
+                });
+                
+                const responsedata = await response.json();
+          
+                if (response.ok) {
+    
+                  setCategorydata(responsedata.categories);  // Store the fetched data in state
+
+                }
+                else{
+                    alert(responsedata.message)
+                }
+    
+              } catch (error) {
+                console.error('Error:', error);
+              }
+            
+          }
+
+
+    useEffect(()=>{
+      getCategory()
+    },[])
+
+    {/* Add Category API Call */}
+    const postCategory = async (e) => {
+    e.preventDefault();
+
+       const categoryData={
       name:categoryname,
-      description:description,
+      description:description
     }
 
-    const addCategory=async ()=>{
-
-          try {
+    try {
       const response = await fetch(`${BASE_URL}/addCategory`, {
         method: "POST",
         headers: {
-    Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
       },
         body: JSON.stringify(categoryData),
       });
@@ -29,7 +66,7 @@ function AdminCategories() {
       const data = await response.json();
       
       if(response.ok){
-        alert("New Category Added")
+        alert("Profile Updated successfully")
         console.log(data);
       }
       else{
@@ -40,6 +77,31 @@ function AdminCategories() {
       console.error("Error uploading product:", error);
     }
   };
+
+  {/* Delete Category API  */}
+  const deleteCategory = async (categoryId) => {
+  try {
+    const response = await fetch(`${BASE_URL}/deleteCategory/${categoryId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`,
+      },
+    });
+
+    const responseData = await response.json();
+
+    if (response.ok) {
+      alert("Category deleted successfully");
+      getCategory(); // Re-fetch the student list after deletion
+    } else {
+      alert(responseData.message);
+    }
+  } catch (error) {
+    console.error("Delete error:", error);
+  }
+};
+
 
 
   return (
@@ -53,7 +115,7 @@ function AdminCategories() {
             </div>
             <div className="w-[500px] flex flex-col">
                 <div className="w-full flex rounded-tl-lg rounded-tr-lg px-3 py-3 text-lg text-white font-bold bg-gradient-to-r from-purple-600 to-pink-600">Add New Category</div>
-                <form className="w-full flex flex-col justify-center py-7 px-3 gap-5 border border-gray-300 shadow-sm" action="">
+                <form onSubmit={postCategory} className="w-full flex flex-col justify-center py-7 px-3 gap-5 border border-gray-300 shadow-sm" action="">
                   
                   <div className="w-full flex flex-col gap-2">
                     <h1 className="font-semibold">Category Name</h1>
@@ -67,7 +129,7 @@ function AdminCategories() {
                       <textarea className="w-full px-2 py-2 outline-0 border border-black rounded-lg" rows={5} name="" id="" placeholder="Provide a detailed Description of this Category ..." onChange={(e)=>{setDescription(e.target.value)}}></textarea>
                     </div>
                   </div>
-                  <button className="w-full text-white font-semibold px-2 py-2 outline-0 rounded-lg bg-gradient-to-r from-purple-600 to-pink-600" onClick={()=>{addCategory()}}>Add Category</button>
+                  <button className="w-full text-white font-semibold px-2 py-2 outline-0 rounded-lg bg-gradient-to-r from-purple-600 to-pink-600">Add Category</button>
 
                 </form>
                 
@@ -79,14 +141,21 @@ function AdminCategories() {
               <h1>Categories Length</h1>
             </div>
            
-           <div className="w-full px-20">
-            <div className="w-full px-5 py-3 flex justify-between rounded-lg border border-gray-300 shadow-sm">
-               <div className="flex flex-col">
-                <h1 className="font-bold">Category Name</h1>
-                <h1>This is ---- category</h1>
-               </div>
-              <button>Delete</button>
-             </div>
+           <div className="w-full flex flex-col gap-4 px-20">
+           {
+            categorydata.length===0 ? <div>No Category</div> :
+             categorydata.map((category,index)=>{
+              return  <div key={index} className="w-full px-5 py-3 flex justify-between items-center rounded-lg border border-gray-300 shadow-sm">
+                    <div className="flex flex-col">
+                    <h1 className="font-bold">{category.name}</h1>
+                    <h1>{category.description}</h1>
+                    </div>
+                    <Trash2 className="w-5 h-5 cursor-pointer hover:scale-[115%]" color="red" onClick={()=>{deleteCategory(category._id)}}/>
+                </div>
+             })  
+           }
+           
+            
            </div>
 
     </div>
