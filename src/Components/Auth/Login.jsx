30e6f9b87e8405d5d070148ea6a978f1b8da40a7
shaan Aslam <shaanaslam0101@gmail.com>
2025-07-19 10:52:07 +0500

    update

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -150,47 +150,47 @@ function Login() {
         <div className='flex flex-col justify-between gap-4 py-5 items-center overflow-y-hidden  max-w-full min-h-screen bg-gradient-to-br from-blue-50 via-indigo-50 to-purple-50'>
 
             <div className='flex flex-col items-center gap-2'>
-              <div className='flex justify-center items-center bg-gradient-to-r from-blue-600 to-purple-600 rounded-full w-16 h-16'>
-                  <GraduationCap className="w-8 h-8 text-white" />
+              <div className='flex justify-center items-center bg-gradient-to-r from-blue-600 to-purple-600 rounded-full w-12 h-12 md:w-16 md:h-16'>
+                  <GraduationCap className="w-7 h-7 md:w-8 md:h-8 text-white" />
               </div>
               
-              <h1 className='text-3xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent'>FindMyTutor</h1>
+              <h1 className='text-2xl md:text-3xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent'>FindMyTutor</h1>
               <h1>Wellcome Back to learning</h1>
             </div>
 
             <div className='flex flex-col w-[33%] h-auto justify-between rounded-lg px-6 py-3 bg-white items-center shadow-lg shadow-gray-200'>
-              <h1 className='text-2xl font-bold py-3'>Sign In</h1>
+              <h1 className='text-2xl font-bold py-1 md:py-3'>Sign In</h1>
               <div className='w-full flex flex-col gap-2 py-3'>
                 <h1 className='flex flex-col font-semibold'>Sign in as:</h1>
-                  <div className='w-full flex basis-1 gap-4 flex-wrap justify-center  items-center'>
-                    <div className={`flex flex-col items-center cursor-pointer hover:scale-[103%] rounded-lg ${studentbg} px-8 py-2 border-2 border-gray-200`} onClick={() => {setStudentbg('bg-blue-100');setTeacherbg('bg-white');setAdminbg('bg-white');setTeacherapi(false);setStudentapi(true);setAdminapi(false)}}>
-                      <BookOpen className="w-5 h-5" />
+                  <div className='w-full flex flex-col px-3 py-0 md:py-1 gap-2 lg:flex lg:flex-row basis-1 gap-2 md:gap-3 lg:gap-5 lg:flex-wrap lg:justify-center lg:items-center'>
+                    <div className={`flex flex-col items-center cursor-pointer hover:scale-[103%] rounded-lg ${studentbg} px-1 py-1 md:px-6 md:py-2 lg:px-8 lg:py-2 border-2 border-gray-200`} onClick={() => {setStudentbg('bg-blue-100');setTeacherbg('bg-white');setAdminbg('bg-white');setTeacherapi(false);setStudentapi(true);setAdminapi(false)}}>
+                      <BookOpen className="w-4 h-4" />
                       <h1>Student</h1>
                     </div>
-                   <div className={`flex flex-col items-center cursor-pointer hover:scale-[103%] rounded-lg ${teacherbg} px-8 py-2 border-2 border-gray-200`} onClick={() => {setTeacherbg('bg-blue-100');setAdminbg('bg-white');setStudentbg('bg-white');setTeacherapi(true);setStudentapi(false);setAdminapi(false)}}>
-                      <Users className="w-5 h-5" />
+                   <div className={`flex flex-col items-center cursor-pointer hover:scale-[103%] rounded-lg ${teacherbg} px-1 py-1 md:px-6 md:py-2 lg:px-8 lg:py-2 border-2 border-gray-200`} onClick={() => {setTeacherbg('bg-blue-100');setAdminbg('bg-white');setStudentbg('bg-white');setTeacherapi(true);setStudentapi(false);setAdminapi(false)}}>
+                      <Users className="w-4 h-4" />
                       <h1>Teacher</h1>
                     </div>
-                    <div className={`flex flex-col items-center cursor-pointer hover:scale-[103%] rounded-lg ${adminbg} px-8 py-2 border-2 border-gray-200`} onClick={() => {setAdminbg('bg-blue-100');setTeacherbg('bg-white');setStudentbg('bg-white');setTeacherapi(false);setStudentapi(false);setAdminapi(true)}}>
-                      <Shield className="w-5 h-5" />
+                    <div className={`flex flex-col items-center cursor-pointer hover:scale-[103%] rounded-lg ${adminbg} px-1 py-1 md:px-6 md:py-2 lg:px-8 lg:py-2 border-2 border-gray-200`} onClick={() => {setAdminbg('bg-blue-100');setTeacherbg('bg-white');setStudentbg('bg-white');setTeacherapi(false);setStudentapi(false);setAdminapi(true)}}>
+                      <Shield className="w-4 h-4" />
                       <h1>Admin</h1>
                     </div>
                   </div>
               </div>
 
-              <form action="" onSubmit={handleSubmit} className='flex flex-col w-full py-3 gap-5 items-center'>
+              <form action="" onSubmit={handleSubmit} className='flex flex-col w-full py-0 md:py-3 gap-2 md:gap-3 lg:gap-5 items-center'>
              
               <div className='w-full flex justify-between items-center px-2 rounded-lg border border-gray-300 bg-gray-100'>
                     <Mail className="h-5 w-5 text-gray-400" />
-                    <input type="email" placeholder='Email Address' className='w-full outline-0 px-3 py-3' onChange={(e)=>{setEmail(e.target.value)}}/>
+                    <input type="email" placeholder='Email Address' className='w-full outline-0 px-3 py-1 md:py-3' onChange={(e)=>{setEmail(e.target.value)}}/>
               </div>
                 
                <div className='w-full flex justify-between items-center px-2 rounded-lg border border-gray-300 bg-gray-100'>
                      <Lock className="h-5 w-5 text-gray-400" />
-                    <input autoComplete='on' type="password" placeholder='Password' className='w-full outline-0 px-3 py-3' onChange={(e)=>{setPassword(e.target.value)}}/>
+                    <input autoComplete='on' type="password" placeholder='Password' className='w-full outline-0 px-3 py-1 md:py-3' onChange={(e)=>{setPassword(e.target.value)}}/>
               </div>
 
-                <button type='submit' className='bg-blue-600 w-full py-3 text-white font-semibold rounded-lg cursor-pointer hover:scale-[102%] hover:bg-blue-700'>Sign In</button>
+                <button type='submit' className='bg-blue-600 w-full py-1 md:py-3 text-white font-semibold rounded-lg cursor-pointer hover:scale-[102%] hover:bg-blue-700'>Sign In</button>
               </form>
 
               <NavLink to="/ForgetPass">
@@ -199,7 +199,7 @@ function Login() {
              
 
                 <hr className='border-s border-gray-300 w-full'/>
-                <div className='py-3'>
+                <div className='py-3 flex flex-col items-center md:flex md:flex-col md:items-center'>
                   <label htmlFor="" className='text-gray-600'>Dont have an account? </label>
                 
                  <NavLink to="/Register">
--- src/Components/Auth/Register.jsx
+++ src/Components/Auth/Register.jsx
@@ -105,58 +105,58 @@ function Register() {
         <div className='flex flex-col justify-between gap-4 py-5 items-center overflow-y-hidden  max-w-full min-h-screen bg-gradient-to-br from-blue-50 via-indigo-50 to-purple-50'>
 
             <div className='flex flex-col items-center gap-2'>
-              <div className='flex justify-center items-center bg-gradient-to-r from-blue-600 to-purple-600 rounded-full w-16 h-16'>
-                  <GraduationCap className="w-8 h-8 text-white" />
+              <div className='flex justify-center items-center bg-gradient-to-r from-blue-600 to-purple-600 rounded-full w-12 h-12 md:w-16 md:h-16'>
+                  <GraduationCap className="w-7 h-7 md:w-8 md:h-8 text-white" />
               </div>
               
-              <h1 className='text-3xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent'>FindMyTutor</h1>
+              <h1 className='text-xl md:text-3xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent'>FindMyTutor</h1>
               <h1>Join our learning community</h1>
             </div>
 
             <div className='flex flex-col w-[33%] h-auto justify-between rounded-lg px-6 py-3 bg-white items-center shadow-lg shadow-gray-200'>
-              <h1 className='text-2xl font-bold py-3'>Create Account</h1>
+              <h1 className='text-xl md:text-2xl font-bold py-1 md:py-3'>Create Account</h1>
               <div className='w-full flex flex-col gap-2 py-3'>
                 <h1 className='flex flex-col font-semibold'>I am a:</h1>
-                  <div className='w-full flex basis-1 gap-4 flex-wrap justify-center  items-center'>
-                    <div className={`flex flex-col items-center rounded-lg ${studentbg} px-12 py-5 border-2 border-gray-200`} onClick={() => {setStudentbg('bg-blue-100');setTeacherbg('bg-white');setTeacherapi(false);setStudentapi(true)}}>
+                  <div className='w-full flex flex-col px-3 py-0 md:py-1 gap-2 md:gap-3 lg:gap-5 lg:flex lg:flex-row basis-1 lg:flex-wrap lg:justify-center lg:items-center'>
+                    <div className={`flex flex-col items-center rounded-lg ${studentbg} px-1 py-1 md:px-6 md:py-2 lg:px-8 lg:py-2 border-2 border-gray-200`} onClick={() => {setStudentbg('bg-blue-100');setTeacherbg('bg-white');setTeacherapi(false);setStudentapi(true)}}>
                       <BookOpen className="w-5 h-5" />
                       <h1>Student</h1>
                     </div>
-                   <div className={`flex flex-col items-center rounded-lg ${teacherbg} px-12 py-5 border-2 border-gray-200`} onClick={() => {setTeacherbg('bg-blue-100');setStudentbg('bg-white');setTeacherapi(true);setStudentapi(false)}}>
+                   <div className={`flex flex-col items-center rounded-lg ${teacherbg} px-1 py-1 md:px-6 md:py-2 lg:px-8 lg:py-2 border-2 border-gray-200`} onClick={() => {setTeacherbg('bg-blue-100');setStudentbg('bg-white');setTeacherapi(true);setStudentapi(false)}}>
                       <Users className="w-5 h-5" />
                       <h1>Teacher</h1>
                     </div>
                   </div>
               </div>
 
-              <form action="" onSubmit={handleSubmit} className='flex flex-col w-full py-3 gap-5 items-center'>
+              <form action="" onSubmit={handleSubmit} className='flex flex-col w-full py-3 gap-2 md:gap-3 lg:gap-5 items-center'>
              
               <div className='w-full flex justify-between items-center px-2 rounded-lg border border-gray-300 bg-gray-100'>
                     <Users className="h-5 w-5 text-gray-400" />
-                    <input type="text" placeholder='Full Name' className='w-full outline-0 px-3 py-3' onChange={(e)=>{setUsename(e.target.value)}}/>
+                    <input type="text" placeholder='Full Name' className='w-full outline-0 px-3 py-1 md:py-3' onChange={(e)=>{setUsename(e.target.value)}}/>
               </div>
 
               <div className='w-full flex justify-between items-center px-2 rounded-lg border border-gray-300 bg-gray-100'>
                     <Mail className="h-5 w-5 text-gray-400" />
-                    <input type="text" placeholder='Email Address' className='w-full outline-0 px-3 py-3' onChange={(e)=>{setEmail(e.target.value)}}/>
+                    <input type="text" placeholder='Email Address' className='w-full outline-0 px-3 py-1 md:py-3' onChange={(e)=>{setEmail(e.target.value)}}/>
               </div>
                 
                <div className='w-full flex justify-between items-center px-2 rounded-lg border border-gray-300 bg-gray-100'>
                      <Lock className="h-5 w-5 text-gray-400" />
-                    <input type="text" placeholder='Password' className='w-full outline-0 px-3 py-3' onChange={(e)=>{setPassword(e.target.value)}}/>
+                    <input type="text" placeholder='Password' className='w-full outline-0 px-3 py-1 md:py-3' onChange={(e)=>{setPassword(e.target.value)}}/>
               </div>
 
               <div className='w-full flex justify-between items-center px-2 rounded-lg border border-gray-300 bg-gray-100'>
                      <Lock className="h-5 w-5 text-gray-400" />
-                    <input type="text" placeholder='Conform Password' className='w-full outline-0 px-3 py-3' onChange={(e)=>{setConfirmpass(e.target.value)}}/>
+                    <input type="text" placeholder='Conform Password' className='w-full outline-0 px-3 py-1 md:py-3' onChange={(e)=>{setConfirmpass(e.target.value)}}/>
               </div>
 
-                <button type='submit' className='bg-blue-600 w-full py-3 text-white font-semibold rounded-lg cursor-pointer hover:scale-[102%] hover:bg-blue-700'>Create Account</button>
+                <button type='submit' className='bg-blue-600 w-full py-1 md:py-3 text-white font-semibold rounded-lg cursor-pointer hover:scale-[102%] hover:bg-blue-700'>Create Account</button>
               </form>
 
 
                 <hr className='border-s border-gray-300 w-full'/>
-                <div className='py-3'>
+                <div className='py-3 flex flex-col items-center md:flex md:flex-col md:items-center'>
                   <label htmlFor="" className='text-gray-600'>Already have an account? </label>
                 
                  <NavLink to="/Login">
